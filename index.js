6a990bad9fa2d41cbb21c2dd8711f2a99f59bf53
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-21 14:13:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    const winningYear = record.find(game => game.result === "W")
+    if (winningYear){
+        return winningYear.year;
+    }else{
+        return undefined
+    }
+}
